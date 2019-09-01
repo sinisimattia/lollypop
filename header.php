73e43ac400099e3f284a6be6a79b314d8c793cd4
Mattia Sinisi <50796596+sinisimattia@users.noreply.github.com>
2019-09-01 19:06:33 +0200

    Removed Google API Key

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 	<script src="<?php echo get_template_directory_uri() ?>/js/layout.js" rel="text/javascript"></script>
 	<script src="<?php echo get_template_directory_uri() ?>/js/feedback.js" rel="text/javascript"></script>
 
-	<script id="API_Script" type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCEZsUjjGEkLUs1CuRhf-cvbbS0obeUhbs"></script>
+	<script id="API_Script" type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
 
 	<script src="<?php echo get_template_directory_uri() ?>/js/maps.js" rel="text/javascript"></script>
 
